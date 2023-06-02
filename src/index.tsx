11f60da0c58417873228edc400eb72582b982a56
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-06-02 17:48:54 +0300

    refactor: add changes

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,12 @@ import {Provider} from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <React.StrictMode>
+    <div>
         {/*для дз 10*/}
         <Provider store={store}>
             <App/>
         </Provider>
-    </React.StrictMode>
+    </div>
 )
 
 reportWebVitals()
