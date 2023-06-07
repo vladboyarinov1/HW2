b72bca56d15aa4419a0c9bdd61047737a170c007
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-06-07 21:32:55 +0300

    feat: add hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -5,6 +5,7 @@ import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
+import {Container, HwTitle} from '../../common/styles/container';
 
 /*
 * 1 - в файле loadingReducer.ts дописать типы и логику
@@ -15,19 +16,25 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector((state: AppStoreType) => state.loading.isLoading)
+    const dispatch = useDispatch()
+
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1000)
+        dispatch(loadingAC(true))
 
         // setTimeout
     }
 
     return (
         <div id={'hw10'}>
-            <div className={s2.hwTitle}>Homework #10</div>
+            <HwTitle className={s2.hwTitle}>Homework #10</HwTitle>
 
-            <div className={s2.hw}>
+            <Container className={s2.hw}>
                 {isLoading ? (
                     <div id={'hw10-loading'}>
                         <Loader/>
@@ -40,7 +47,7 @@ const HW10 = () => {
                         Set loading...
                     </SuperButton>
                 )}
-            </div>
+            </Container>
         </div>
     )
 }
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,14 +2,15 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): any => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {...state, isLoading: action.isLoading}
         default:
             return state
     }
 }
+
 
 type LoadingActionType = {
     type: 'CHANGE_LOADING'
